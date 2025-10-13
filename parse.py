838edea0d0e593291f52673786324379203b98dc
Rylie Pavlik <rylie.pavlik@collabora.com>
2025-10-13 12:00:04 -0500

    Parser has args and types now

--- parse.py
+++ parse.py
@@ -24,12 +24,51 @@ class ExtractorStateMachine:
         self.lines: list[str] = []
         self.method: Optional[str] = None
         self.params: list[str] = []
+        self.indent = "    "
+        self.url = ""
 
     def _handle_param(self, t: Token):
-        self.params = ["**kwargs"]
-        pass
+        if not t.children:
+            self.params = ["**kwargs"]
+            return
+        if t.content == "**none**":
+            self.params = []
+            return
+        text_nodes = [child.content for child in t.children if child.type == "text"]
 
-    def handle_token(self, t: Token):
+        param_name = text_nodes[1]
+        type_info = "".join(text_nodes[2:])
+        is_optional = "optional" in type_info
+        param_type = "Any"
+        default = ""
+
+        # print(len(text_nodes))
+        if "string" in type_info:
+            param_type = "str"
+        elif "integer" in type_info or "int," in type_info:
+            param_type = "int"
+        elif "boolean" in type_info:
+            param_type = "bool"
+            # if "default is false" in type_info:
+            #     default = " = False"
+        elif "dict" in type_info or "key/value" in type_info:
+            param_type = "dict"
+        elif "array" in type_info:
+            param_type = "list"
+        else:
+            print(
+                "Could not figure out param type:",
+                f"'{type_info}'",
+                "from parent node",
+                f"'{t.content}'",
+            )
+        if is_optional:
+            param_type = f"Optional[{param_type}]"
+            default = " = None"
+
+        self.params.append(f"{param_name}: {param_type}{default}")
+
+    def handle_token(self, stem: str, t: Token):
         if self.state == State.NORMAL:
             if t.type == "heading_open":
                 self.state = State.IN_HEADING
@@ -44,13 +83,9 @@ class ExtractorStateMachine:
                 return
 
             self.method = caseswitcher.to_snake(t.content)
+            anchor = t.content.lower()
+            self.url = f"https://docs.kanboard.org/v1/api/{stem}/#{anchor}"
 
-            # self.lines.extend(
-            #     [
-            #         f"  def {method}(self, **kwargs): ...",
-            #         f"  def {method}_async(self, **kwargs): ...",
-            #     ]
-            # )
         elif self.state == State.EXPECT_PARAM_LIST:
             if t.type == "bullet_list_open":
                 self.state = State.IN_PARAM_LIST
@@ -61,21 +96,30 @@ class ExtractorStateMachine:
                 self._handle_param(t)
             elif t.type == "bullet_list_close":
                 # done with params
-                args = ", ".join(self.params)
+                args = ""
+                if self.params:
+                    args = "*, " + ", ".join(self.params)
+
                 self.lines.extend(
                     [
-                        f"  def {self.method}(self, ..., {args}): ...",
-                        f"  def {self.method}_async(self, ..., {args}): ...",
+                        f"\n{self.indent}# {self.url}",
+                        f"{self.indent}def {self.method}(self, {args}): ...",
+                        f"\n{self.indent}# {self.url}",
+                        f"{self.indent}def {self.method}_async(self, {args}): ...",
                     ]
                 )
                 self.state = State.NORMAL
 
-    def handle_file_contents(self, md: MarkdownIt, contents: str):
+    def handle_file_contents(self, md: MarkdownIt, stem: str, contents: str):
 
         tokens = md.parse(contents)
 
         for t in tokens:
-            self.handle_token(t)
+            self.handle_token(stem, t)
+
+    def inject_comment(self, comment_text):
+
+        self.lines.append(f"\n{self.indent}# {comment_text}")
 
 
 # def _parse_file(md: MarkdownIt, fn):
@@ -131,12 +175,16 @@ if __name__ == "__main__":
     # methods = []
     for fn in Path("content/en/v1/api/").glob("*_procedures.md"):
         section = fn.stem
-        sm.lines.append(f"\n  # {section}")
+        sm.inject_comment(section)
 
         with open(fn, "r", encoding="utf-8") as fp:
             contents = fp.read()
-        sm.handle_file_contents(md, contents)
+        sm.handle_file_contents(md, fn.stem, contents)
         # methods.extend(_parse_file(md, fn))
 
-    print("class Client:")
-    print("\n".join(sm.lines))
+    with open("stubs.pyi", "w", encoding="utf-8") as fp:
+        fp.write("class Client:\n")
+        fp.write("\n".join(sm.lines))
+        fp.write("\n")
+    # print()
+    # print("\n".join(sm.lines))
